b2fa8357fc80aec5e59d9381d864d0ea56152f31
Christopher Hudgin <chudgin@moonlight.lan>
2023-03-21 11:18:19 -0400

    commit

---
+++ os/unix/history/unixprogenv/exercises/chapter_2/2.2/footnote/fstest.f
@@ -0,0 +1,13 @@
+      PROGRAM FSTEST
+
+      WRITE(*,10) '      PROGRAM HELLO'
+      WRITE(*,10) ''
+      WRITE(*,10) '      WRITE(*,10) ''hello, world'''
+      WRITE(*,10) ' 10   FORMAT(A)'
+      WRITE(*,10) ''
+      WRITE(*,10) '      STOP'
+      WRITE(*,10) '      END'
+ 10   FORMAT(A)
+
+      STOP
+      END
